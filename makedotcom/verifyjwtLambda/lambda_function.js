d76cdafd0ca83f33b9a444991ab0ab9da5d4e8a8
szytel <nate@nextreason.com>
2025-03-07 12:43:22 -0800

    added clarity to the configuration files

--- makedotcom/verifyjwtLambda/lambda_function.js
+++ makedotcom/verifyjwtLambda/lambda_function.js
@@ -1,13 +1,13 @@
 import { JwtVerifier } from "aws-jwt-verify";
 
 const verifier = JwtVerifier.create({
-  issuer: "https://id.eu.nextreason.com/", // set this to the expected "iss" claim on your JWTs
-  audience: "https://webhook.site/workflows-enrich-test", // set this to the expected "aud" claim on your JWTs
-  jwksUri: "https://id.eu.nextreason.com/.well-known/jwks.json", // set this to the JWKS uri from your OpenID configuration
+  issuer: "https://NEXT_IDENTITY_URL/", // set this to the expected "iss" claim on your JWTs
+  audience: "AUD_CLAIM", // set this to the expected "aud" claim on your JWTs
+  jwksUri: "https://NEXT_IDENTITY_URL/.well-known/jwks.json", // set this to the JWKS uri from your OpenID configuration
 });
 
 try {
-  const payload = await verifier.verify("eyJraWQeyJhdF9oYXNoIjoidk...");
+  const payload = await verifier.verify("token");
   console.log("Token is valid. Payload:", payload);
 } catch {
   console.log("Token not valid!");
